9cb7821f2f31c484ded9fb11957f879c28030335
samuelisimo <ramueljb@gmail.com>
2025-07-11 19:08:27 -0600

    no lint

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+  eslint: {
+    // ADVERTENCIA: Esto permitirá que tu proyecto se compile incluso si tiene errores de ESLint.
+    ignoreDuringBuilds: true,
+  },
   // Any other configurations you might have...
 
   // This is the crucial part for proxying requests:
@@ -8,7 +13,7 @@ const nextConfig = {
     return [
       {
         source: '/api/:path*', // Catches requests starting with /api/
-        destination: 'http://127.0.0.1:5000/api/:path*', // Forwards them to your Flask backend
+        destination: 'https://nextstep-backend-1-0-0.onrender.com/api/:path*', // Forwards them to your Flask backend
       },
     ];
   },
